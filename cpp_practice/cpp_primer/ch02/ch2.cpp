894ae337f05f35d2188851fac5dfbbaf0e25defd
Guanghui Wei <jerrywei1985@hotmail.com>
2020-12-28 12:53:06 +0800

    add c++ primer ch03

--- cpp_practice/cpp_primer/ch02/ch2.cpp
+++ cpp_practice/cpp_primer/ch02/ch2.cpp
@@ -24,6 +24,8 @@ int p_2_18() {
 
 int p_2_19() {
     int i = 0;
+    // constexpr int j = 0;
+    // int *p = j;
     const int &r1 = i;
     std::cout << r1 << std::endl;
     i = 10;
@@ -31,9 +33,31 @@ int p_2_19() {
     return 0;
 }
 
+int p_2_33() {
+    int i = 0, &r = i;
+    auto a = r;
+    const int ci = i, &cr = ci;
+    auto b = ci;
+    auto c = cr;
+    auto d = &i;
+    auto e = &ci;
+    const auto f = ci;
+    auto &g = ci;
+    const auto &j = 42;
+    a = 42;
+    b = 42;
+    c = 42;
+    std::cout << *e << std::endl;
+    // d = 42;
+    e = 42; // 0是可以的，因为内存安全
+    // g = 42;
+    return 0;
+}
+
 int main() {
     // return p_2_14();
     // return p_2_17();
     // return p_2_18();
-    return p_2_19();
+    // return p_2_19();
+    return p_2_33();
 }
